7fe9f48b48d4755579301cb41965062f052dbebd
BaconButPro <147504175+ron12373@users.noreply.github.com>
2025-06-21 11:41:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -75,8 +75,8 @@ def initiate_stage(stage_id, session):
             return None
         return None
 
-    with ThreadPoolExecutor(max_workers=150) as executor:
-        futures = [executor.submit(single_request) for _ in range(150)]
+    with ThreadPoolExecutor(max_workers=600 as executor:
+        futures = [executor.submit(single_request) for _ in range(600)]
         for future in as_completed(futures):
             result = future.result()
             if result:
@@ -102,8 +102,8 @@ def validate_stage(token, referrer, session):
             return None
         return None
 
-    with ThreadPoolExecutor(max_workers=150) as executor:
-        futures = [executor.submit(single_request) for _ in range(150)]
+    with ThreadPoolExecutor(max_workers=600) as executor:
+        futures = [executor.submit(single_request) for _ in range(600)]
         for future in as_completed(futures):
             result = future.result()
             if result:
@@ -131,8 +131,8 @@ def authenticate(validated_tokens, session):
             return None
         return None
 
-    with ThreadPoolExecutor(max_workers=200) as executor:
-        futures = [executor.submit(single_request) for _ in range(200)]
+    with ThreadPoolExecutor(max_workers=600) as executor:
+        futures = [executor.submit(single_request) for _ in range(600)]
         for future in as_completed(futures):
             if future.result():
                 return True
