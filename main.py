ae1b61f7ba918d9a931bf70b206cec8e1c104461
BaconButPro <147504175+ron12373@users.noreply.github.com>
2025-07-28 10:13:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def bypass_redirect():
         return jsonify({"status": "error", "message": "Missing URL parameter"}), 400
 
     bypass_url = f"http://bypass.html?url={original_url}"
-    return jsonify({"status": "success", "url": bypass_url}), 200
+    return jsonify({"status":"success","result": bypass_url}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
